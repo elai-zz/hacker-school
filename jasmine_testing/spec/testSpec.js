d01da5bec888cb09832f11612b086fde47133003
Minz <minzeycat@gmail.com>
2014-06-18 11:29:48 -0400

    renamed mockyList to mocky

--- jasmine_testing/spec/testSpec.js
+++ jasmine_testing/spec/testSpec.js
@@ -1,5 +1,5 @@
-var _ = mockyList.any;
-var $ = mockyList.param;
+var _ = mocky.any;
+var $ = mocky.param;
 
 function foo() {
 	alert('hi');
@@ -9,7 +9,7 @@ function foo1() {
 	return 1;
 }
 
-var c = mockyList(
+var c = mocky(
 	[2,3], 
 	[1,2],
 	[3 , function() {
@@ -19,7 +19,7 @@ var c = mockyList(
 	[_, "wow!"]
 );
 
-var d = mockyList(
+var d = mocky(
 	[$, function(n) {
 	return n+n}]);
 
@@ -66,68 +66,68 @@ describe("Calling c('apple')", function() {
 
 describe("Calling arrayMatch", function() {
     it("should return null", function() {
-        expect(mockyList.arrayMatch(["a","b","c"],[])).toEqual(null);
+        expect(mocky.arrayMatch(["a","b","c"],[])).toEqual(null);
 	});
 });
 
 describe("Calling arrayMatch", function() {
     it("should return 2", function() {
-        expect(mockyList.arrayMatch(["a","b","c"], [{"k": ["a","b","c"], "v": 2}, {"k": ["a","b","d"], "v": 3}])).toEqual(2);
+        expect(mocky.arrayMatch(["a","b","c"], [{"k": ["a","b","c"], "v": 2}, {"k": ["a","b","d"], "v": 3}])).toEqual(2);
 	});
 });
 
 describe("Calling arrayMatch", function() {
     it("should return 2", function() {
-        expect(mockyList.arrayMatch(["a","b","c"], [{"k": ["a","b","d"], "v": 3},{"k": ["a","b","c"], "v": 2},])).toEqual(2);
+        expect(mocky.arrayMatch(["a","b","c"], [{"k": ["a","b","d"], "v": 3},{"k": ["a","b","c"], "v": 2},])).toEqual(2);
 	});
 });
 
 describe("Calling arrayMatch", function() {
     it("should return 2", function() {
-        expect(mockyList.arrayMatch([], [{"k": [], "v": 2},{"k": ["a","b","c"], "v": 88},])).toEqual(2);
+        expect(mocky.arrayMatch([], [{"k": [], "v": 2},{"k": ["a","b","c"], "v": 88},])).toEqual(2);
 	});
 });
 
 describe("Calling arrayMatch", function() {
     it("should return null", function() {
-        expect(mockyList.arrayMatch([], [{"k": ["a","b","c"], "v": 88},])).toEqual(null);
+        expect(mocky.arrayMatch([], [{"k": ["a","b","c"], "v": 88},])).toEqual(null);
 	});
 });
 
 describe("Calling arrayMatch", function() {
     it("should return null", function() {
-        expect(mockyList.arrayMatch(["a","b","c"], [{"k": ["a","b","d"], "v": 3}])).toEqual(null);
+        expect(mocky.arrayMatch(["a","b","c"], [{"k": ["a","b","d"], "v": 3}])).toEqual(null);
 	});
 });
 
 describe("Calling arrayMatch", function() {
     it("should return 3", function() {
-        expect(mockyList.arrayMatch(["a","b","c"], [{"k": [_], "v": 3}])).toEqual(3);
+        expect(mocky.arrayMatch(["a","b","c"], [{"k": [_], "v": 3}])).toEqual(3);
 	});
 });
 
 
 describe("Calling arrayMatch", function() {
  	it("should return 4", function() {
-     	expect(mockyList.arrayMatch(["a","b","d","c"], [{"k": [_], "v": 4}, {"k": ["a","b","b","c"], "v": 3}])).toEqual(4);
+     	expect(mocky.arrayMatch(["a","b","d","c"], [{"k": [_], "v": 4}, {"k": ["a","b","b","c"], "v": 3}])).toEqual(4);
  	});
 });
 
 describe("Calling arrayMatch with two any's at beginning", function() {
  	it("should return 4", function() {
-     	expect(mockyList.arrayMatch(["a","b","d","c"], [{"k": [_, "d","c"], "v": 4}])).toEqual(4);
+     	expect(mocky.arrayMatch(["a","b","d","c"], [{"k": [_, "d","c"], "v": 4}])).toEqual(4);
  	});
 });
 
 describe("Calling arrayMatch with one any's at beginning", function() {
  	it("should return 4", function() {
-     	expect(mockyList.arrayMatch(["a","b","d","c"], [{"k": [_, "c"], "v": 4}])).toEqual(4);
+     	expect(mocky.arrayMatch(["a","b","d","c"], [{"k": [_, "c"], "v": 4}])).toEqual(4);
  	});
 });
 
 describe("Calling arrayMatch with one any's at beginning", function() {
  	it("should return bee", function() {
-     	expect(mockyList.arrayMatch(["a","b","d","d","e"], [{"k": [_, "c"], "v": "busy bee"},{"k": [_, "d", _], "v": "bee"}])).toEqual("bee");
+     	expect(mocky.arrayMatch(["a","b","d","d","e"], [{"k": [_, "c"], "v": "busy bee"},{"k": [_, "d", _], "v": "bee"}])).toEqual("bee");
  	});
 }); 
 
@@ -135,25 +135,25 @@ describe("Calling arrayMatch with one any's at beginning", function() {
 
 describe("Calling arrayMatch with mixed param and any", function() {
  	it("should return 3", function() {
-     	expect(mockyList.arrayMatch(["a","b","d","d","e"], [{"k": ["c", _], "v": 4},{"k": [$, "d", _], "v": 3}])).toEqual(3);
+     	expect(mocky.arrayMatch(["a","b","d","d","e"], [{"k": ["c", _], "v": 4},{"k": [$, "d", _], "v": 3}])).toEqual(3);
  	});
 }); 
 
 describe("Calling arrayMatch with mixed param and any", function() {
  	it("should return 4", function() {
-     	expect(mockyList.arrayMatch(["a","b","d","d","e"], [{"k": [_,"c",_], "v": 2},{"k": [_, "d", $], "v": 4}])).toEqual(4);
+     	expect(mocky.arrayMatch(["a","b","d","d","e"], [{"k": [_,"c",_], "v": 2},{"k": [_, "d", $], "v": 4}])).toEqual(4);
  	});
 }); 
 
 describe("Calling arrayMatch with mixed param and any", function() {
  	it("should return 4", function() {
-     	expect(mockyList.arrayMatch(["a","b","d","d","e"], [{"k": [_,"e", _, _], "v": 2},{"k": [_, "d", $], "v": 4}])).toEqual(4);
+     	expect(mocky.arrayMatch(["a","b","d","d","e"], [{"k": [_,"e", _, _], "v": 2},{"k": [_, "d", $], "v": 4}])).toEqual(4);
  	});
 }); 
 
 describe("Calling arrayMatch with mixed param and any", function() {
  	it("should return 4", function() {
-     	expect(mockyList.arrayMatch(["a","b","d","d","e"], [{"k": [_,"e", $], "v": 2},{"k": [_, "d", $], "v": 4}])).toEqual(4);
+     	expect(mocky.arrayMatch(["a","b","d","d","e"], [{"k": [_,"e", $], "v": 2},{"k": [_, "d", $], "v": 4}])).toEqual(4);
  	});
 }); 
 
@@ -162,7 +162,7 @@ describe("Calling arrayMatch with mixed param and any", function() {
 // unit tests now using functions
 describe("Calling arrayMatch", function() {
     it("should return 4", function() {
-        expect(mockyList.arrayMatch(["a","b","c"], [
+        expect(mocky.arrayMatch(["a","b","c"], [
 		{"k": ["a","b","d"], "v": 2},
 		{"k": ["a","b","c"], "v": function() {
 			var v = 1+1;
@@ -172,7 +172,7 @@ describe("Calling arrayMatch", function() {
 
 describe("Calling arrayMatch", function() {
     it("should return 2", function() {
-        expect(mockyList.arrayMatch(["a","b","c"], [
+        expect(mocky.arrayMatch(["a","b","c"], [
 		{"k": ["a","c","d"], "v": 3},
 		{"k": ["a", $], "v": function(n) {
 			return n.length;}}])).toEqual(2);
@@ -181,7 +181,7 @@ describe("Calling arrayMatch", function() {
 
 describe("Calling arrayMatch", function() {
     it("should return 1", function() {
-        expect(mockyList.arrayMatch(["a","b","c"], [
+        expect(mocky.arrayMatch(["a","b","c"], [
 		{"k": ["a","c","d"], "v": 3},
 		{"k": [_,"b", $], "v": function(n) {
 			return n.length;}}])).toEqual(1);
--- jasmine_testing/src/mocky.js
+++ jasmine_testing/src/mocky.js
@@ -1,48 +1,210 @@
 (function(exports) {
 	
-	var mockyList = createMatcher;
-	mockyList.createMatcher = createMatcher;
-	exports.mockyList = mockyList;
+	var mocky = createMatcher;
+	mocky.createMatcher = createMatcher;
+	mocky.arrayMatch = arrayMatch;
+	exports.mocky = mocky;
 	
-	function createMatcher(patterns) {
+	mocky.any = (function () {
+		function Any() {}
+		return new Any();
+	})();
+	
+	mocky.param = (function () {
+		function Param() {}
+		return new Param();
+	})();
+	
+	function createMatcher() {
+		var patterns = arguments;
 		
 		return function(n) {
-
-			if (patterns[n] == undefined)
-			{
-				if (patterns["$"] != undefined) return give("$", patterns, n);
 			
-				else if (patterns["_"] != undefined) return give("_", patterns);
+			var patternsL = [];
+			var anyPattern;
+			var paramExist = false; // set it to false at the beginning as assumption
 			
+		    for (var i = 0; i < patterns.length; i++) {
+				
+				if (patterns[i][0] == mocky.any) {
+					anyPattern = patterns[i][1];
+				}
+				
 				else {
-					throw "non exhaustive pattern matching";
+					if (patterns[i][0] == mocky.param) {
+						paramExist = patterns[i][1];
+					}
+					newObj = {"k": patterns[i][0], "v" : patterns[i][1]};		
+					patternsL.push(newObj); 	
+				}		
+			}
+			
+			if (anyPattern && paramExist) {
+				throw "ambiguous pattern any and param";
+			}
+			
+			/* now breaking it up to different cases */
+			
+			if (n.isArray) {
+				arrayMatch(n, patternsL);
+			}
+			
+			else {
+				for (var i = 0; i < patternsL.length; i++) {
+				
+					if (patternsL[i]["k"] == n)
+					{
+						if (typeof(patternsL[i]["v"]) === "function"){
+						
+							return patternsL[i]["v"](n);
+						}
+						else {
+							return patternsL[i]["v"];
+						}
+					}
+				}
+			
+			}
+			
+			if (anyPattern)
+			{
+				if (typeof(anyPattern) === "function") {
+					return anyPattern();
+				}
+				else {
+					return anyPattern;
 				}
 			}
-		
-			else { 
-				return give(n, patterns);
+			
+			else if (paramExist) {	
+				var tmp = paramExist;
+				return tmp(n);
+			
 			}
-
+		
+			// throwing an error
+			else {
+				throw "non exhaustive pattern matching";
+			} 
 		}
-
 	}
 	
-	var give = function(sym, patterns, n) {
+	function returnFuncOrAtom(ret, ob) {
 		
-		if (typeof(patterns[sym]) === "function")
-		{
-			var temp = patterns[sym];
-			var param = ((sym == "$") ? n : sym);
-			return temp(param);
-		}  
+		if (typeof(ret) === "function") return ret(ob);
+		else return ret;
 		
-		// for cases like func 2 = 1
-		else {
-			var foo = patterns[sym];
-			return foo;
+	}
+	function arrayMatch(n, p) {
+		for (var i = 0; i < p.length; i++) {
+			var currPatternKey = p[i]["k"];
+			var currPatternVal = p[i]["v"];
+			var j = 0;
+			
+			// we modify n in the process depending on pattern
+			// this is to keep the beginning state of n
+			var copyN = n; 
+			
+			// in case we see a param char
+			var paramMatch;
+			
+			if (currPatternKey[j] == mocky.any || 
+				currPatternKey[j] == mocky.param) {
+					
+				if (currPatternKey.length == 1){
+					// we see a [_] or a [$]
+					return returnFuncOrAtom(currPatternVal, n);
+
+				} 
+				
+				else if (currPatternKey[j] == mocky.param) {
+					// we are seeing something like [$,_]
+					if (currPatternKey[1] == mocky.any) {
+						return returnFuncOrAtom(currPatternVal, n[1]);
+					}
+					
+				}
+				
+				else {
+					// we are seeing something like [_,$]
+					if (currPatternKey[1] == mocky.param) {
+						paramMatch = n.slice(1); //we have a param
+						return returnFuncOrAtom(currPatternVal, paramMatch);
+					}
+					
+					else if (currPatternKey[1] != mocky.any) {
+						// we also want to see something like [_,c]
+						// where we need to search for the index in n
+						// slice it out and process it
+						var nextToken = currPatternKey[1];
+						var ind = n.indexOf(nextToken);
+						n = n.slice(ind);
+						currPatternKey = currPatternKey.slice(1);
+					}
+					
+					else {
+						// we also want to see something like [_,_,c]
+						// this we want to take off the beginning part until
+						// we get just the c. 
+						// this would work on [_,_,$] as well
+						while (currPatternKey[1] == mocky.any) {
+							currPatternKey = currPatternKey.slice(1);
+							n = n.slice(1);
+						}
+
+						// // we are seeing something like [_,_,$]
+						// while (currPatternKey[1] == mocky.any) {
+						// 	n = n.slice(1);
+						// 	currPatternKey = currPatternKey.slice(1);		
+						// }
+						// paramMatch = n;
+						// return returnFuncOrAtom(currPatternVal, paramMatch);
+					}	
+				}
+							
+			}
+			
+			// if somehow, the current pattern is longer than n
+			// then we would just break out of the iteration
+			if (currPatternKey.length > n.length) {
+				n = copyN;
+				continue;
+			}
+			
+			// we assume that the input is going to be same or longer than pattern key
+			// while j is within range, otherwise, we can get undefined === undefined
+			
+			// as soon as we can see a _, we don't see more atoms. 
+			// entering this while loop, we know it must be a non param, any.
+			while (j < currPatternKey.length && (currPatternKey[j] === n[j] || 
+				currPatternKey[j] == mocky.any || 
+				currPatternKey[j] == mocky.param)) {
+				
+				if (currPatternKey[j] == mocky.param) {
+					// as soon as we see a param, we can stop and match. 
+					paramMatch = n.slice(j);
+					return returnFuncOrAtom(currPatternVal, paramMatch);
+				}
+					
+				else if (currPatternKey[j] == mocky.any) {
+					if (j+1 == currPatternKey.length) {
+						return returnFuncOrAtom(currPatternVal, n);
+					} // return now because all is matched
+				}
+	
+				j++;
+			}
+
+			// exact match found
+			if (currPatternKey.length == j && n.length == j) {
+				return returnFuncOrAtom(currPatternVal, n);
+			}
+			
+			n = copyN;
+			
 		}
-		
+		mocky
+		throw "non exhaustive pattern matching";
 	}
 	
 })(this);
-
